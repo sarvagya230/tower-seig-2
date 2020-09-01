eefe229219de48caab0fdbefd47c7280eaa5ad82
sarvagya230 <68740017+sarvagya230@users.noreply.github.com>
2020-09-01 10:06:08 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,44 @@
+class box
+{
+    constructor(x,y)
+    {
+        this.x=x;
+        this.y=y;
+        this.width=50
+        this.height=80
+        this.body = Matter.Bodies.rectangle(this.x,this.y,this.width,this.height,{restitution:0.3,isStatic:false})
+        this.visibility=255
+        Matter.World.add(world,this.body)
+
+    }
+    display(color)
+    {
+        var pos = this.body.position 
+        push()
+        translate(pos.x,pos.y)
+        rectMode(CENTER)
+        if (this.body.speed>3)
+        {
+            Matter.World.remove(world,this.body)
+            this.visibility=this.visibility-5
+            push()
+            tint(255,this.visibility)
+            rectMode(CENTER)
+            rect(0,0,this.width,this.height)
+            pop()
+        }
+        if (color===a)
+        {
+            fill(156, 150, 250)
+        }
+        else 
+        {
+
+        fill(color)
+        }
+        rect(0,0,this.width,this.height)
+        pop()
+
+
+    }
+}
